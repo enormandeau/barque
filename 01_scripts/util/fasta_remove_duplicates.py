e5bee4ec5a57992cca0a4518c50442b514c10799
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-07 16:07:22 -0400

    Add fasta_remove_duplicates.py

---
+++ 01_scripts/util/fasta_remove_duplicates.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""Keep only unique sequences, regardless of sequence name
+
+USAGE:
+    python fasta_split.py input_file output_file
+"""
+
+# Importing modules
+import sys
+
+# Defining classes
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+# Defining functions
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with open(input_file) as f:
+        sequence = ""
+        name = ""
+        begun = False
+        for line in f:
+            line = line.strip()
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, sequence)
+                name = line.replace(">", "")
+                sequence = ""
+                begun = True
+            else:
+                sequence += line
+        yield Fasta(name, sequence)
+
+# Parse user input
+try:
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+except:
+    print __doc__
+    sys.exit(1)
+
+# Set of seen sequences
+seen_sequences = set()
+
+# Iterate through sequences and write to files
+with open(output_file, "w") as outf:
+    for s in fasta_iterator(input_file):
+        if not s.sequence in seen_sequences:
+            seen_sequences.add(s.sequence)
+            s.write_to_file(outf)
