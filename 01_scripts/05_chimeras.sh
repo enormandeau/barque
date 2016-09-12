29d590bb2636d7df956b766a48bf36bcb2cadce3
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-12 16:03:44 -0400

    Add output formats to usearch uchime

--- 01_scripts/05_chimeras.sh
+++ 01_scripts/05_chimeras.sh
@@ -5,7 +5,7 @@
 ls -1 06_amplicons_regrouped/*.fasta | parallel ./01_scripts/util/fasta_remove_duplicates.py {} {}.unique
 
 # Use regrouped amplicons to find chimeras with uchime
-ls -1 06_amplicons_regrouped/*.fasta.unique | parallel usearch -uchime_denovo {} -uchimeout {.}.uchime \>\& {.}.uchime.message.small
+ls -1 06_amplicons_regrouped/*.fasta.unique | parallel usearch -uchime_denovo {} -uchimeout {}.uchime -chimeras {}.chimeras -uchimealns {}.alignments \>\& {.}.uchime.message.small
 
 # Remove them from single samples
 
