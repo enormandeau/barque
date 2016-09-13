c585a33d5010c6fe5ba0f8e5bdcbd5d4c40cfb38
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-13 10:18:00 -0400

    Run usearch on all samples with proper database

--- 01_scripts/09_usearch.sh
+++ 01_scripts/09_usearch.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
+# Compile databases if needed
+#ls -1 99_databases/*.fasta | parallel usearch -makeudb_usearch {} -output {.}.udb
+
 # Find best hit in COI database using usearch
-for i in 06_amplicons_regrouped/all_coi*.fasta; do echo $i; usearch -usearch_local $i -db 99_databases/coi_db.udb -id 0.9 -maxaccepts 10 -maxrejects 100 -strand both -blast6out 07_blast_results/$(basename $i.coi_db) -top_hit_only -query_cov 0.5; done
+for amplicon in $(grep -v "^#" primers.csv | awk '{print $1}')
+do
+    database=$(grep -v "^#" primers.csv | grep $amplicon primers.csv | awk '{print $5}').udb
+    echo "$database"
+
+    # Treat each sample
+    for sample in $(ls -1 05_split_by_amplicon/*"$amplicon"*.fastq.gz)
+    do
+        fastq=$(basename "$sample")
+        fasta="${fastq%.fastq.gz}".fasta
+
+        # Create fasta file
+        echo
+        echo "#############################"
+        echo "Creating temporary fasta file"
+        ./01_scripts/util/fastq_to_fasta.py 05_split_by_amplicon/"$fastq" 05_split_by_amplicon/"$fasta"
+
+#-TIME-HACK-#
+        head -1000 05_split_by_amplicon/"$fasta" > 05_split_by_amplicon/"$fasta".temp
+        mv 05_split_by_amplicon/"$fasta".temp 05_split_by_amplicon/"$fasta"
+#-TIME-HACK-#
+
+        # Run usearch
+        echo "Running usearch on $fasta with database $database"
+        usearch -usearch_local 05_split_by_amplicon/"$fasta" -db 99_databases/"$database" -id 0.9 \
+        -maxaccepts 10 -maxrejects 100 -strand both -blast6out \
+        07_blast_results/"${fasta%.fasta}"."${database%.udb}" -top_hit_only -query_cov 0.5
+
+        # Cleanup fasta file
+        echo "Removing temporary fasta file"
+        rm 05_split_by_amplicon/"$fasta"
+    done
+done
