2b3fcc7f975c549d17a58c9cc5bd8e033990772f
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-13 11:07:54 -0400

    Cleanup of usearch script

--- 01_scripts/06_usearch.sh
+++ 01_scripts/06_usearch.sh
@@ -7,9 +7,12 @@
 for amplicon in $(grep -v "^#" primers.csv | awk '{print $1}')
 do
     database=$(grep -v "^#" primers.csv | grep $amplicon primers.csv | awk '{print $5}').udb
-    echo "$database"
+    echo "#######################"
+    echo "# database: $database"
+    echo "#######################"
 
     # Treat each sample
+#-DIRTY-HACK-# (head and tail)
     for sample in $(ls -1 05_split_by_amplicon/*"$amplicon"*.fastq.gz)
     do
         fastq=$(basename "$sample")
@@ -17,23 +20,21 @@ do
 
         # Create fasta file
         echo
-        echo "#############################"
-        echo "Creating temporary fasta file"
         ./01_scripts/util/fastq_to_fasta.py 05_split_by_amplicon/"$fastq" 05_split_by_amplicon/"$fasta"
 
-#-TIME-HACK-#
-        head -1000 05_split_by_amplicon/"$fasta" > 05_split_by_amplicon/"$fasta".temp
+        #-dirty-hack-# (keep only top sequences)
+        head -2000 05_split_by_amplicon/"$fasta" > 05_split_by_amplicon/"$fasta".temp
         mv 05_split_by_amplicon/"$fasta".temp 05_split_by_amplicon/"$fasta"
-#-TIME-HACK-#
 
         # Run usearch
         echo "Running usearch on $fasta with database $database"
+
+        # usearch local (fast, best in our case)
         usearch -usearch_local 05_split_by_amplicon/"$fasta" -db 99_databases/"$database" -id 0.9 \
-        -maxaccepts 10 -maxrejects 100 -strand both -blast6out \
-        07_blast_results/"${fasta%.fasta}"."${database%.udb}" -top_hit_only -query_cov 0.5
+            -maxaccepts 10 -maxrejects 100 -strand both -blast6out \
+            07_blast_results/"${fasta%.fasta}"."${database%.udb}" -top_hit_only -query_cov 0.5
 
         # Cleanup fasta file
-        echo "Removing temporary fasta file"
         rm 05_split_by_amplicon/"$fasta"
     done
 done
