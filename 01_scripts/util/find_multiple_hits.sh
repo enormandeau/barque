0a49c9f0e2648900e6795d85bf719b46ced61802
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-22 14:49:14 -0400

    Add 01_scripts/util/find_multiple_hits.sh

---
+++ 01_scripts/util/find_multiple_hits.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# From usearch results, identify cases of multiple hits
+
+# Goal: To create a list of species to remove from the database
+
+for file in 07_blast_results_multiple_hits/*.bold
+do
+    for i in $(awk '{print $1}' $file | sort | uniq -c | sort -nr | head | awk '{print $2}')
+    do
+        grep $i $file | awk '{print $2}' | sort | uniq -c | sort -nr | awk '{print $2}'
+        echo
+    done
+done | perl -pe 's/\n/_newline_/g' |
+    perl -pe 's/_newline__newline_/\n/g' |
+    grep _newline_ |
+    sort |
+    uniq -c |
+    sort -nr |
+    awk '{print $1,$2}' |
+    perl -pe 's/ / time(s):\n/' |
+    perl -pe 's/_newline_/\n/g' |
+    perl -pe 's/^(\d)/\n\1/' > multiple_hits.txt
