97c5901196cb40a38b49f3569c7477f09ab58596
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-27 10:33:51 -0400

    Parallelize 02_merge.sh

--- 01_scripts/02_merge.sh
+++ 01_scripts/02_merge.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Merge
-for i in $(ls -1 03_trimmed/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u)
-do
-    echo "Merging: $i"
-    flash -t 1 -z -O -m 30 -M 280 03_trimmed/"$i"R1_001.fastq.gz 03_trimmed/"$i"R2_001.fastq.gz > /dev/null
-    mv out.extendedFrags.fastq.gz 04_merged/"$i"merged.fastq.gz
-done
+# Parallel version
+ls -1 03_trimmed/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u | parallel flash -t 1 -z -O -m 30 -M 280 03_trimmed/{}R1_001.fastq.gz 03_trimmed/{}R2_001.fastq.gz --to-stdout \> 04_merged/{}merged.fastq.gz
 
-# Cleanup
-rm out.*
+## Merge not parallel
+#for i in $(ls -1 03_trimmed/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u)
+#do
+#    echo "Merging: $i"
+#    flash -t 1 -z -O -m 30 -M 280 03_trimmed/"$i"R1_001.fastq.gz 03_trimmed/"$i"R2_001.fastq.gz > /dev/null
+#    mv out.extendedFrags.fastq.gz 04_merged/"$i"merged.fastq.gz
+#done
+
+## Cleanup
+#rm out.*
+
