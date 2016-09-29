b3ccb14033c91f2fc325a5c2f83123fb286e5b31
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-29 11:34:04 -0400

    Update splitting by amplicons

--- 01_scripts/03_split_amplicons.sh
+++ 01_scripts/03_split_amplicons.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ls -1 04_merged/*.fastq.gz | parallel ./01_scripts/split_amplicons_one_file.py 04_merged/{/} primers.csv iupac.csv 05_split_by_amplicon
+
+# Global variables
+INFOFOLDER="02_info"
+MERGEDFOLDER="06_merged"
+SPLITFOLDER="07_split_amplicons"
+
+# Split amplicons
+ls -1 "$MERGEDFOLDER"/*.fastq.gz |
+    parallel ./01_scripts/util/split_amplicons_one_file.py "$MERGEDFOLDER"/{/} \
+    "$INFOFOLDER"/primers.csv \
+    "$INFOFOLDER"/iupac.csv \
+    "$SPLITFOLDER"
