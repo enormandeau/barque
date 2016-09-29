cc93739757428a1e8dcac10aa409c85453c98a85
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-29 11:27:33 -0400

    Minor (cleanup in 02_merge.sh)

--- 01_scripts/02_merge.sh
+++ 01_scripts/02_merge.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
+# Global variables
 TRIMMEDFOLDER="05_trimmed"
 MERGEDFOLDER="06_merged"
 
 # Parallel version
 ls -1 "$TRIMMEDFOLDER"/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u | parallel flash -t 1 -z -O -m 30 -M 280 "$TRIMMEDFOLDER"/{}R1_001.fastq.gz "$TRIMMEDFOLDER"/{}R2_001.fastq.gz --to-stdout \> "$MERGEDFOLDER"/{}merged.fastq.gz
-
-## Merge not parallel
-#for i in $(ls -1 "$TRIMMEDFOLDER"/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u)
-#do
-#    echo "Merging: $i"
-#    flash -t 1 -z -O -m 30 -M 280 "$TRIMMEDFOLDER"/"$i"R1_001.fastq.gz "$TRIMMEDFOLDER"/"$i"R2_001.fastq.gz > /dev/null
-#    mv out.extendedFrags.fastq.gz "$MERGEDFOLDER"/"$i"merged.fastq.gz
-#done
-
-## Cleanup
-#rm out.*
-
