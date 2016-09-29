bf517861defe361ab1f0691db9cba7b7356fc60b
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-29 11:23:18 -0400

    Update sequence merging

--- 01_scripts/02_merge.sh
+++ 01_scripts/02_merge.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+TRIMMEDFOLDER="05_trimmed"
+MERGEDFOLDER="06_merged"
+
 # Parallel version
-ls -1 03_trimmed/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u | parallel flash -t 1 -z -O -m 30 -M 280 03_trimmed/{}R1_001.fastq.gz 03_trimmed/{}R2_001.fastq.gz --to-stdout \> 04_merged/{}merged.fastq.gz
+ls -1 "$TRIMMEDFOLDER"/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u | parallel flash -t 1 -z -O -m 30 -M 280 "$TRIMMEDFOLDER"/{}R1_001.fastq.gz "$TRIMMEDFOLDER"/{}R2_001.fastq.gz --to-stdout \> "$MERGEDFOLDER"/{}merged.fastq.gz
 
 ## Merge not parallel
-#for i in $(ls -1 03_trimmed/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u)
+#for i in $(ls -1 "$TRIMMEDFOLDER"/ | perl -pe 's/R[12].*\.fastq.gz//' | sort -u)
 #do
 #    echo "Merging: $i"
-#    flash -t 1 -z -O -m 30 -M 280 03_trimmed/"$i"R1_001.fastq.gz 03_trimmed/"$i"R2_001.fastq.gz > /dev/null
-#    mv out.extendedFrags.fastq.gz 04_merged/"$i"merged.fastq.gz
+#    flash -t 1 -z -O -m 30 -M 280 "$TRIMMEDFOLDER"/"$i"R1_001.fastq.gz "$TRIMMEDFOLDER"/"$i"R2_001.fastq.gz > /dev/null
+#    mv out.extendedFrags.fastq.gz "$MERGEDFOLDER"/"$i"merged.fastq.gz
 #done
 
 ## Cleanup
