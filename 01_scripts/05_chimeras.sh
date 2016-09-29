b8cd0d73fe4426ff13378f998c7d99e87bbdfb01
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-29 11:39:12 -0400

    Update chimera detection

--- 01_scripts/05_chimeras.sh
+++ 01_scripts/05_chimeras.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Find and remove chimeras
 
+# Global variables
+CHIMERASFOLDER="08_chimeras"
+
 # Remove duplicated sequences
-ls -1 06_amplicons_regrouped/*.fasta | parallel ./01_scripts/util/fasta_remove_duplicates.py {} {}.unique
+ls -1 "$CHIMERASFOLDER"/*.fasta |
+    parallel ./01_scripts/util/fasta_remove_duplicates.py {} {}.unique
 
 # Use regrouped amplicons to find chimeras with uchime
-ls -1 06_amplicons_regrouped/*.fasta.unique | parallel usearch -uchime_denovo {} -uchimeout {}.uchime -chimeras {}.chimeras -uchimealns {}.alignments \>\& {.}.uchime.message.small
+ls -1 "$CHIMERASFOLDER"/*.fasta.unique |
+    parallel usearch -uchime_denovo {} -uchimeout {}.uchime -chimeras {}.chimeras \
+    -uchimealns {}.alignments \>\& {.}.uchime.message.small
 
 # Remove them from single samples
 
