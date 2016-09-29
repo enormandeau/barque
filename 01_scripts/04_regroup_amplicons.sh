e56b11c0d4724c52ea2e4be659b6a08cc48f4f36
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-29 11:34:26 -0400

    Update regrouping by amplicon for chimeras

--- 01_scripts/04_regroup_amplicons.sh
+++ 01_scripts/04_regroup_amplicons.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 # Regroup all samples per amplicon
 
-for amplicon in $(grep -v "^#" primers.csv | awk '{print $1}')
+# Global variables
+INFOFOLDER="02_info"
+SPLITFOLDER="07_split_amplicons"
+CHIMERAFOLDER="08_chimeras"
+
+# Regroup all samples per amplicon
+for amplicon in $(grep -v "^#" "$INFOFOLDER"/primers.csv | awk '{print $1}')
 do
     echo "Treating: $amplicon"
-    REGROUPED=06_amplicons_regrouped/all_"$amplicon".fastq
+    REGROUPED="$CHIMERAFOLDER"/all_"$amplicon".fastq
 
     # Create empty fastq file
     rm "$REGROUPED" 2>/dev/null
     touch "$REGROUPED"
 
     # Concatenate all samples
-    for i in $(ls -1 05_split_by_amplicon | grep "$amplicon")
+    for i in $(ls -1 "$SPLITFOLDER" | grep "$amplicon")
     do
-        gunzip -c 05_split_by_amplicon/"$i" >> "$REGROUPED"
+        gunzip -c "$SPLITFOLDER"/"$i" >> "$REGROUPED"
     done
 
     # fastq_to_fasta
