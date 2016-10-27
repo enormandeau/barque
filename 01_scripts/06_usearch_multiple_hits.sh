00dbc26203b3354cb3551440b63026f82a10f3c3
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-10-27 12:49:50 -0400

    Update usearch scripts for v1.0

--- 01_scripts/06_usearch_multiple_hits.sh
+++ 01_scripts/06_usearch_multiple_hits.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
-# Compile databases if needed
-#ls -1 99_databases/*.fasta | parallel usearch -makeudb_usearch {} -output {.}.udb
+# Global variables
+INFOFOLDER="02_info"
+SPLITFOLDER="07_split_amplicons"
+USEARCHFOLDER="09_usearch_multiple_hits"
 
 # Find best hit in COI database using usearch
-for amplicon in $(grep -v "^#" primers.csv | awk '{print $1}')
+for amplicon in $(grep -v "^#" "$INFOFOLDER"/primers.csv | awk '{print $1}')
 do
-    database=$(grep -v "^#" primers.csv | grep $amplicon primers.csv | awk '{print $5}').udb
+    database=$(grep -v "^#" "$INFOFOLDER"/primers.csv | grep $amplicon | awk '{print $5}').udb
     echo "#######################"
     echo "# database: $database"
     echo "#######################"
 
     # Treat each sample
-    for sample in $(ls -1 05_split_by_amplicon/*"$amplicon"*.fastq.gz)
+    for sample in $(ls -1 "$SPLITFOLDER"/*"$amplicon"*.fastq.gz)
     do
         # File names
         fastq=$(basename "$sample")
-        fasta="${fastq%.fastq.gz}".fasta
+        fasta="${fastq%.fastq.gz}"_usearch_multihits.fasta
 
         # Create fasta file
         echo
-        ./01_scripts/util/fastq_to_fasta.py 05_split_by_amplicon/"$fastq" 05_split_by_amplicon/"$fasta"
-
-        ## -dirty-hack- # (keep only top sequences)
-        #head -2000 05_split_by_amplicon/"$fasta" > 05_split_by_amplicon/"$fasta".temp
-        #mv 05_split_by_amplicon/"$fasta".temp 05_split_by_amplicon/"$fasta"
+        ./01_scripts/util/fastq_to_fasta.py "$SPLITFOLDER"/"$fastq" "$SPLITFOLDER"/"$fasta"
 
         # Run usearch
         echo "Running usearch on $fasta with database $database"
 
         # usearch local (fast, best in our case)
-        usearch -usearch_local 05_split_by_amplicon/"$fasta" -db 99_databases/"$database" -id 0.95 \
+        usearch -usearch_local "$SPLITFOLDER"/"$fasta" -db 03_databases/"$database" -id 0.95 \
             -maxaccepts 10 -maxrejects 50 -strand both -blast6out \
-            07_blast_results_multiple_hits/"${fasta%.fasta}"."${database%.udb}" -top_hits_only -query_cov 0.5
+            "$USEARCHFOLDER"/"${fasta%.fasta}"."${database%.udb}" -top_hits_only -query_cov 0.5
 
         # Cleanup fasta file
-        rm 05_split_by_amplicon/"$fasta"
+        rm "$SPLITFOLDER"/"$fasta"
     done
 done
