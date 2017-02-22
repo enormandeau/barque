99dfcd7100d01cf56b456681175e4a05b8dde435
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-02-22 10:06:08 -0500

    Add scripts to redesign primers in-silico

---
+++ 01_scripts/util/missed_primers_01_find.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Find missed forward primers
+zcat 07_split_amplicons/*not_found* | grep -E "^[ACTG]+$" | head -10000 | cut -c -20 | sort | uniq -c | sort -nr > forward_primers_not_found
+
+
+# Find missed reverse primers
+zcat 07_split_amplicons/*not_found* | grep -E "^[ACTG]+$" | head -10000 | grep -oE ".{20}$" | sort  | rev | tr "actgACTG" "tgacTGAC" | uniq -c | sort -nr > reverse_primers_not_found
+
+# Edit files for missed_primers_02.py
+vim -p forward_primers_not_found reverse_primers_not_found 02_info/primers.csv
---
+++ 01_scripts/util/missed_primers_02_design.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""Design degenerate primers from a file with one sequence per line
+
+Note: The sequences must not be degenerate and have the same length
+
+Usage:
+    ./01_scripts/util/missed_primers_02_design.py input_file
+"""
+
+# Modules
+import sys
+
+# Get user input
+try:
+    input_file = sys.argv[1]
+except:
+    print __doc__
+    sys.exit(1)
+
+# Functions
+
+# Iupac characters
+iupac = {
+        "A": "A",
+        "C": "C",
+        "G": "G",
+        "T": "T",
+        "AG": "R",
+        "CT": "Y",
+        "CG": "S",
+        "AT": "W",
+        "GT": "K",
+        "AC": "M",
+        "CGT": "B",
+        "AGT": "D",
+        "ACT": "H",
+        "ACG": "V",
+        "ACGT": "N",
+        }
+
+# Read all sequences in a list
+sequences = []
+with open(input_file) as infile:
+    for line in infile:
+        seq = line.strip()
+        sequences.append(seq)
+
+# Find possible nucleotides at each position
+degenerate = []
+for i in xrange(len(sequences[0])):
+    nucleotides = "".join(sorted(list(set([x[i] for x in sequences]))))
+    degenerate.append(nucleotides)
+
+# Create and print degenerate primer
+primer = []
+for n in degenerate:
+    primer.append(iupac[n])
+
+print "".join(primer)
