8ace4f8ce6ab849ebc4975ab7f753b633b11337d
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-03-10 09:54:18 -0500

    Also clean 14_non_annotated_sequences/* for small test

--- 01_scripts/util/test_small.sh
+++ 01_scripts/util/test_small.sh
@@ -3,7 +3,7 @@
 # NOTE: Works only on the developper's computer
 
 # Cleanup temporary data and result directories
-rm 04_data/* 05_trimmed/* 06_merged/* 07_split_amplicons/* 08_chimeras/* 09_usearch_multiple_hits/* 10_results_multiple_hits/* 11_usearch/* 12_results/* 13_read_dropout/* most_present_non_annotated_sequences.fasta multiple_hits.txt 2>/dev/null
+rm 04_data/* 05_trimmed/* 06_merged/* 07_split_amplicons/* 08_chimeras/* 09_usearch_multiple_hits/* 10_results_multiple_hits/* 11_usearch/* 12_results/* 13_read_dropout/* most_present_non_annotated_sequences.fasta multiple_hits.txt 2>/dev/null 14_non_annotated_sequences/*
 
 # Import small dataset, database, and primers
 cp /home/labolb/temp.backup/barque_test_dataset/*.gz 04_data
