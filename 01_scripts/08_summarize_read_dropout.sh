5c397d40210b084ac3546f90e665719d0e016135
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-03-10 09:53:42 -0500

    Minor (format 01_scripts/08_summarize_read_dropout.sh)

--- 01_scripts/08_summarize_read_dropout.sh
+++ 01_scripts/08_summarize_read_dropout.sh
@@ -5,33 +5,52 @@
 step="04_data"
 cd "$step"
 echo -e "$step\t$step" > ../13_read_dropout/$step
-for i in *R1*.gz; do echo -e $(echo "$i" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1); done | sort -V >> ../13_read_dropout/$step
+for i in *R1*.gz
+do
+    echo -e $(echo "$i" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
+done | sort -V >> ../13_read_dropout/$step
 cd ..
 
 # 05_trimmed
 step="05_trimmed"
 cd "$step"
 echo -e "$step\t$step" > ../13_read_dropout/$step
-for i in *R1*.gz; do echo -e $(echo "$i" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1); done | sort -V >> ../13_read_dropout/$step
+for i in *R1*.gz
+do
+    echo -e $(echo "$i" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
+done | sort -V >> ../13_read_dropout/$step
 cd ..
 
 # 06_merged
 step="06_merged"
 cd "$step"
 echo -e "$step\t$step" > ../13_read_dropout/$step
-for i in *.gz; do echo -e $(echo "$i" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1); done | sort -V >> ../13_read_dropout/$step
+for i in *.gz
+do
+    echo -e $(echo "$i" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
+done | sort -V >> ../13_read_dropout/$step
 cd ..
 
 # 07_split_amplicons
 step="07_split_amplicons"
 cd "$step"
-for i in $(grep -v "^#" ../02_info/primers.csv | awk '{print $1}'); do echo -e "$step\t$step" > ../13_read_dropout/"$step"_"$i"; for j in $(ls -1 *merged_"$i"*.gz | grep "$i"); do echo -e $(echo "$j" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$j" | wc -l) / 4 | bc | cut -d "." -f 1); done | sort -V >> ../13_read_dropout/"$step"_"$i"; done
+for i in $(grep -v "^#" ../02_info/primers.csv | awk '{print $1}')
+do
+    echo -e "$step\t$step" > ../13_read_dropout/"$step"_"$i"
+    for j in $(ls -1 *merged_"$i"*.gz | grep "$i")
+    do
+        echo -e $(echo "$j" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$j" | wc -l) / 4 | bc | cut -d "." -f 1)
+    done | sort -V >> ../13_read_dropout/"$step"_"$i"
+done
 cd ..
 
 # 12_results
 step="12_results"
 cd "$step"
-for i in $(grep -v "^#" ../02_info/primers.csv | awk '{print $1}'); do echo -e "$step\t$step" > ../13_read_dropout/"$step"_"$i"; awk 'NR==1 {for (i=1; i<=NF; i++) {headers[i]=$i}} NR>1 {for (i=1; i<=NF; i++) {sums[i]+=$i}} END {for (i=4; i<=NF; i++) {print headers[i]"\t"sums[i]}}' "$i"_species_results.csv | sort -V >> ../13_read_dropout/"$step"_"$i"; done
+for i in $(grep -v "^#" ../02_info/primers.csv | awk '{print $1}')
+do
+    echo -e "$step\t$step" > ../13_read_dropout/"$step"_"$i"; awk 'NR==1 {for (i=1; i<=NF; i++) {headers[i]=$i}} NR>1 {for (i=1; i<=NF; i++) {sums[i]+=$i}} END {for (i=4; i<=NF; i++) {print headers[i]"\t"sums[i]}}' "$i"_species_results.csv | sort -V >> ../13_read_dropout/"$step"_"$i"
+done
 cd ..
 
 # Put everything together
