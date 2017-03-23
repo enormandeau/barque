48078205511c5b413b2e0578738bbf725f58c7fc
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-03-23 13:12:04 -0400

    Use `sort -g` instead of `-V` for OSX portability

--- 01_scripts/08_summarize_read_dropout.sh
+++ 01_scripts/08_summarize_read_dropout.sh
@@ -8,7 +8,7 @@ echo -e "Sample,$step" > ../13_read_dropout/$step
 for i in *R1*.gz
 do
     echo -e $(echo "$i" | cut -d "_" -f 1)","$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
-done | sort -V >> ../13_read_dropout/$step
+done | sort -g >> ../13_read_dropout/$step
 cd ..
 
 # 05_trimmed
@@ -18,7 +18,7 @@ echo -e "$step" > ../13_read_dropout/$step
 for i in *R1*.gz
 do
     echo -e $(echo "$i" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
-done | sort -V | awk '{print $2}' >> ../13_read_dropout/$step
+done | sort -g | awk '{print $2}' >> ../13_read_dropout/$step
 cd ..
 
 # 06_merged
@@ -28,7 +28,7 @@ echo -e "$step" > ../13_read_dropout/$step
 for i in *.gz
 do
     echo -e $(echo "$i" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
-done | sort -V | awk '{print $2}' >> ../13_read_dropout/$step
+done | sort -g | awk '{print $2}' >> ../13_read_dropout/$step
 cd ..
 
 # 07_split_amplicons
@@ -40,7 +40,7 @@ do
     for j in $(ls -1 *merged_"$i"*.gz | grep "$i")
     do
         echo -e $(echo "$j" | cut -d "_" -f 1)"\t"$(echo $(gunzip -c "$j" | wc -l) / 4 | bc | cut -d "." -f 1)
-    done | sort -V | awk '{print $2}' >> ../13_read_dropout/"$step"_"$i"
+    done | sort -g | awk '{print $2}' >> ../13_read_dropout/"$step"_"$i"
 done
 cd ..
 
@@ -49,7 +49,7 @@ step="12_results"
 cd "$step"
 for i in $(grep -v "^#" ../02_info/primers.csv | awk -F "," '{print $1}')
 do
-    echo -e "$step" > ../13_read_dropout/"$step"_"$i"; awk -F "," 'NR==1 {for (i=1; i<=NF; i++) {headers[i]=$i}} NR>1 {for (i=1; i<=NF; i++) {sums[i]+=$i}} END {for (i=5; i<=NF; i++) {print headers[i]"\t"sums[i]}}' "$i"_species_table.csv | sort -V | awk '{print $2}' >> ../13_read_dropout/"$step"_"$i"
+    echo -e "$step" > ../13_read_dropout/"$step"_"$i"; awk -F "," 'NR==1 {for (i=1; i<=NF; i++) {headers[i]=$i}} NR>1 {for (i=1; i<=NF; i++) {sums[i]+=$i}} END {for (i=5; i<=NF; i++) {print headers[i]"\t"sums[i]}}' "$i"_species_table.csv | sort -g | awk '{print $2}' >> ../13_read_dropout/"$step"_"$i"
 done
 cd ..
 
