7fa5a9d268413f429b4b08a4aa554a661a6598d9
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-03-29 15:32:14 -0400

    Use vsearch for chimera detection

--- 01_scripts/05_chimeras.sh
+++ 01_scripts/05_chimeras.sh
@@ -6,16 +6,12 @@ CHIMERASFOLDER="08_chimeras"
 
 # Remove duplicated sequences
 ls -1 "$CHIMERASFOLDER"/*.fasta |
-    parallel ./01_scripts/util/fasta_remove_duplicates.py {} {}.unique
+    parallel vsearch --derep_fulllength {} --output {}.unique --sizeout
 
 # Use regrouped amplicons to find chimeras with uchime
 ls -1 "$CHIMERASFOLDER"/*.fasta.unique |
-    parallel usearch -uchime_denovo {} -uchimeout {}.uchime -chimeras {}.chimeras \
-    -uchimealns {}.alignments \>\& {.}.uchime.message.small
+    parallel vsearch --uchime_denovo {} --chimeras {}.chimeras \
+        --nonchimeras {}.nonchimeras --borderline {}.borderline
 
 # Copy results in 12_results
 cp "$CHIMERASFOLDER"/*.fasta.unique.chimeras 12_results
-
-# Cleanup
-#rm 08_chimeras/*.fasta
-#rm 08_chimeras/*.fasta.unique
