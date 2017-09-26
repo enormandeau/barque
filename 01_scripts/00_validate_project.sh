1508e7e08783a323958c36df40c75906fd745d9d
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-09-26 12:08:52 -0400

    Validate database and sample files are present

---
+++ 01_scripts/00_validate_project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Validate project before launching Barque
+
+# Global variables
+DATABASE_FOLDER="03_databases"
+DATA_FOLDER="04_data"
+
+# Databases used in primers.csv file are present (.fasta.gz)
+for database in $(cat 02_info/primers.csv | grep -v "^#" | cut -d "," -f 6)
+do
+    if ! [ -e "$DATABASE_FOLDER"/"$database".fasta -o -e "$DATABASE_FOLDER"/"$database".fasta.gz ]
+    then
+        echo -e "\n"BARQUE ERROR: Database file for \("$database"\) not found in "$DATABASE_FOLDER"
+        exit 1
+    fi
+done
+
+# Data files (.fastq.gz or .fq.gz) are present in 04-data
+if ! ls -1 "$DATA_FOLDER"/*.f*q.gz > /dev/null
+then
+    echo -e "\n"BARQUE ERROR: No sample found in "$DATA_FOLDER"
+    exit 1
+fi
