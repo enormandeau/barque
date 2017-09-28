0cadb38094d664b0dedb06b217c16a4436c49e4c
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-09-28 10:25:09 -0400

    Correct docstring in fasta_format_non_chimera.py

--- 01_scripts/util/fasta_format_non_chimera.py
+++ 01_scripts/util/fasta_format_non_chimera.py
@@ -2,7 +2,7 @@
 """Convert vsearch unique fasta to barque unique fasta
 
 Usage:
-    python fasta_to_unique_fasta.py inputFile outputFile
+    python fasta_format_non_chimera.py inputFile outputFile
 
 Where:
     inputFile is a .fasta or a .fasta.gz file from vsearch
