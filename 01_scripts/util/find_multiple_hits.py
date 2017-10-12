62d51b56c19176ac827fa0a9e9f5b7142bd50f8c
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-10-12 09:45:46 -0400

    Improve multiple hits reporting

--- 01_scripts/util/find_multiple_hits.py
+++ 01_scripts/util/find_multiple_hits.py
@@ -40,11 +40,23 @@ for result_file in result_files:
             l = line.strip().split()
             sequence = l[0]
             species = l[1]
-            hits[sequence].append(species)
+            similarity = float(l[2])
+            hits[sequence].append((similarity, species))
 
         for k, v in hits.items():
             if len(set(v)) > 1:
+                # Keep only these who have an the highest similarity
+                v = sorted(v, reverse=True)
+                max_similarity = v[0][0]
+                v = [x for x in v if x[0] == max_similarity]
+
+                # Remove similarity
+                v = [x[1] for x in v]
+
+                # Sort species in a predictable manner
                 sp_group = tuple(sorted(list(set(v))))
+
+                # Add species to species_dict
                 species_dict[sp_group].append(k)
 
 # Count occurences of multiple hits
@@ -57,5 +69,5 @@ sorted_multiple_hits = sorted(multiple_hits, reverse=True)
 
 # Write results to file
 with myopen(outputFile, "wt") as outfile:
-    for species in sorted_multiple_hits: 
+    for species in sorted_multiple_hits:
         outfile.write(str(species[0]) + "," + species[1] + "\n")
---
+++ 01_scripts/util/reduce_multiple_hits.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""Reduce the table of multiple hits by group of shared species
+
+Usage:
+    ./01_scripts/util/reduce_multiple_hits.py inputFile outputFile
+"""
+
+# Modules
+from collections import defaultdict
+import sys
+
+# Parsing user input
+try:
+    inputFile = sys.argv[1]
+    outputFile = sys.argv[2]
+except:
+    print(__doc__)
+    sys.exit(1)
+
+# Reduce
+groups = list()
+all_species = set()
+species_dict = defaultdict(list)
+with open(inputFile) as infile:
+    for line in infile:
+        l = line.strip().split(",")
+        count, species = l
+        species = species.split(";")
+        groups.append((count, species))
+
+        for s in species:
+            all_species.add(s)
+            species_dict[s].append((count, species))
+
+for sp in all_species:
+    species_group = []
+
+    for n, g in enumerate(groups):
+        if sp in g[1]:
+            # Combine all groups that contain a species
+            species_group.append(groups.pop(n))
+
+    # Compute new some and new species list
+    count = sum([int(x[0]) for x in species_group])
+    species = [x[1] for x in species_group]
+
+    # Flatten, make unique, and sort
+    species = sorted(list(set([val for sublist in species for val in sublist])))
+
+    # Add new compound group to list
+    groups = [(count, species)] + groups
+
+groups = sorted(groups, reverse=True)
+groups = [str(g[0]) + "," + ";".join(g[1]) + "\n" for g in groups]
+
+with open(outputFile, "w") as outfile:
+    for g in groups:
+        outfile.write(g)
