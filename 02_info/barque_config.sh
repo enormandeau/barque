05a581177ea9d5e167e48bc5a7e52ae813a2e2a6
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-10-30 09:36:08 -0400

    Keep 1000->500 frequent sequences for annotation
    The NCBI blast server was not able to finish the jobs because they required too much computation time.

--- 02_info/barque_config.sh
+++ 02_info/barque_config.sh
@@ -29,4 +29,4 @@ MIN_HIT_LENGTH=100          # Minimum vsearch hit length to keep in results (int
 MIN_HITS_SAMPLE=1           # Minimum number of hits a species must have in at least one sample
                             #   to keep in results (int, 1+)
 # Non-annotated reads
-NUM_NON_ANNOTATED_SEQ=1000  # Number of unique most-frequent non-annotated reads to keep (int, 1+)
+NUM_NON_ANNOTATED_SEQ=500  # Number of unique most-frequent non-annotated reads to keep (int, 1+)
