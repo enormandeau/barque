7097913cb6f0dd64547838eeedcb94ac3334a595
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-10-15 11:10:47 -0400

    Similarity threshold for non-annotated species

--- 01_scripts/10_report_species_for_non_annotated_sequences.py
+++ 01_scripts/10_report_species_for_non_annotated_sequences.py
@@ -4,11 +4,12 @@ on NCBI, use this script to report the number of non annotated sequences
 for each species (or annotation) found on NCBI.
 
 Usage:
-    ./01_scripts/10_report_species_for_non_annotated_sequences.py AlignmentFile OutputFile
+    ./01_scripts/10_report_species_for_non_annotated_sequences.py AlignmentFile OutputFile threshold
 
 Where:
     AlignmentFile is the file downloaded from NCBI after the blast
     OutputFile is the name of the output file in .csv format
+    threshold is a similarity percentage (integer, 0 to 100, default=90)
 """
 
 # Modules
@@ -23,6 +24,12 @@ except:
     print(__doc__)
     sys.exit(1)
 
+# Default threshold to 90%
+try:
+    threshold = int(sys.argv[3])
+except:
+    threshold = 90
+
 # Prepare dictionary of species (or annotations)
 species_dict = defaultdict(int)
 
@@ -35,13 +42,23 @@ line_num = 0
 while line_num < len(lines):
     line = lines[line_num]
     if line.startswith("Query="):
+        skip = False
         count = int(line.strip().split("_")[3])
+        percent = lines[line_num + 8].split(" ")[-1]
+
+        if int(percent.replace("%", "")) < threshold:
+            skip = True
+
         line_num += 1
         line = lines[line_num]
         while not line.startswith("Query="):
+            if skip:
+                break
+
             line_num += 1
             line = lines[line_num]
             if line.startswith(">"):
+                print(line)
                 species = " ".join(line.split(" ")[1:3])
                 species_dict[species] += count
                 break
