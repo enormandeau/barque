1c2384de672aa45927ff8055075589a680043e82
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-10-16 09:32:23 -0400

    Use Fasta and fasta_iterator

--- 01_scripts/util/format_laetitia_db.py
+++ 01_scripts/util/format_laetitia_db.py
@@ -12,7 +12,48 @@ Name format:
 import gzip
 import sys
 
+# Classes
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+    def __repr__(self):
+        return self.name + " " + self.sequence[:31]
+
 # Functions
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with myopen(input_file) as f:
+        sequence = ""
+        name = ""
+        begun = False
+
+        for line in f:
+            line = line.strip()
+
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, sequence)
+
+                name = line[1:]
+                sequence = ""
+                begun = True
+
+            else:
+                sequence += line
+
+        if name != "":
+            yield Fasta(name, sequence)
+
 def myopen(_file, mode="rt"):
     if _file.endswith(".gz"):
         return gzip.open(_file, mode=mode)
@@ -29,15 +70,14 @@ except:
     sys.exit(1)
 
 # Process db
-with myopen(input_db) as infile:
-    with myopen(output_db, "wt") as outfile:
-        for line in infile:
-            if line.startswith(">"):
-                l = line.strip().split(";")
-                family_name = l[0].split("=")[1]
-                species_name = l[1].split("=")[1].split(".")[0]
-                taxon = family_name + "_" + species_name.replace(" ", "_")
-                taxon = taxon.replace("###", "unknown_unknown")
-                outfile.write(">" + taxon + "\n")
-            else:
-                outfile.write(line)
+sequences = fasta_iterator(input_db)
+
+with myopen(output_db, "wt") as outfile:
+    for s in sequences:
+        l = s.name.strip().split(";")
+        family_name = l[0].split("=")[1]
+        species_name = l[1].split("=")[1].split(".")[0]
+        taxon = family_name + "_" + species_name.replace(" ", "_")
+        taxon = taxon.replace("###", "unknown_unknown")
+        s.name = taxon
+        s.write_to_file(outfile)
