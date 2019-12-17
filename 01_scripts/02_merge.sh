c5bfce9f231e9ecad3e796d547f4dfc3fc05b421
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-12-17 10:35:42 -0500

    BugFix: Samples with R1 or R2 in name

--- 01_scripts/02_merge.sh
+++ 01_scripts/02_merge.sh
@@ -10,8 +10,8 @@ TRIMMED_FOLDER="05_trimmed"
 MERGED_FOLDER="06_merged"
 
 # Parallelize on all trimmed files
-ls -1 -S "$TRIMMED_FOLDER"/*_R1_*.fastq.gz |
-    perl -pe 's/R[12].*\.fastq.gz//' |
+ls -1 -S "$TRIMMED_FOLDER"/*_R1_001.fastq.gz |
+    perl -pe 's/_R[12]_001\.fastq.gz/_/' |
     parallel -j "$NCPUS" flash -t 1 -z -m "$MIN_OVERLAP" -M "$MAX_OVERLAP" \
         {}R1_001.fastq.gz {}R2_001.fastq.gz \
         --to-stdout \> "$MERGED_FOLDER"/{/}merged.fastq.gz
