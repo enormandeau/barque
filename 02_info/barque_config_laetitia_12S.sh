0a4f04f0ba9e76998b65895360f7ac02bc9380de
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-01-27 09:23:12 -0500

    Update config for laetitia

--- 02_info/barque_config_laetitia_12S.sh
+++ 02_info/barque_config_laetitia_12S.sh
@@ -12,7 +12,7 @@ PRIMER_FILE="02_info/primers.csv" # File with PCR primers information
 SKIP_DATA_PREP=0            # 1 to skip data preparation steps, 0 to run full pipeline
 
 # Filtering with Trimmomatic
-CROP_LENGTH=100             # Cut reads to this length after filtering
+CROP_LENGTH=80             # Cut reads to this length after filtering
 
 # Merging reads with flash
 MIN_OVERLAP=20              # Minimum number of overlapping nucleotides to merge reads (int, 1+)
@@ -28,10 +28,10 @@ SKIP_CHIMERA_DETECTION=1    # 0 to search for chimeras (RECOMMENDED), 1 to skip 
 # vsearch
 MAX_ACCEPTS=20              # Accept at most this number of sequences before stoping search (int, 1+)
 MAX_REJECTS=20              # Reject at most this number of sequences before stoping search (int, 1+)
-QUERY_COV=0.6               # At least that proportion of the sequence must match the database (float, 0-1)
+QUERY_COV=0.5               # At least that proportion of the sequence must match the database (float, 0-1)
 
 # Filters
-MIN_HIT_LENGTH=20          # Minimum vsearch hit length to keep in results (int, 1+)
+MIN_HIT_LENGTH=16          # Minimum vsearch hit length to keep in results (int, 1+)
 MIN_HITS_SAMPLE=1           # Minimum number of hits a species must have in at least one sample
                             #   to keep in results (int, 1+)
 # Non-annotated reads
