f75fd1ccdf724ac33750d776eda91ceb2c2aa623
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-01-28 09:29:16 -0500

    Put QUERY_COV back to 0.6

--- 02_info/barque_config_laetitia_12S.sh
+++ 02_info/barque_config_laetitia_12S.sh
@@ -28,7 +28,7 @@ SKIP_CHIMERA_DETECTION=1    # 0 to search for chimeras (RECOMMENDED), 1 to skip 
 # vsearch
 MAX_ACCEPTS=20              # Accept at most this number of sequences before stoping search (int, 1+)
 MAX_REJECTS=20              # Reject at most this number of sequences before stoping search (int, 1+)
-QUERY_COV=0.5               # At least that proportion of the sequence must match the database (float, 0-1)
+QUERY_COV=0.6               # At least that proportion of the sequence must match the database (float, 0-1)
 
 # Filters
 MIN_HIT_LENGTH=16          # Minimum vsearch hit length to keep in results (int, 1+)
