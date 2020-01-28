554eb61c9ac91a005dc803c64b2633a9a85059aa
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-01-28 09:30:10 -0500

    Parallelize dropout counts

--- 01_scripts/08_summarize_read_dropout.sh
+++ 01_scripts/08_summarize_read_dropout.sh
@@ -8,7 +8,7 @@ echo -e "Sample,$step" > ../10_read_dropout/$step
 for i in *R1_001.fastq.gz
 do
     echo -e $(echo "$i" | cut -d "_" -f 1)","$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
-done | sort -t "," -k1,1 -V >> ../10_read_dropout/$step
+done | sort -t "," -k1,1 -V >> ../10_read_dropout/$step &
 cd ..
 
 # 05_trimmed
@@ -18,7 +18,7 @@ echo -e "$step" > ../10_read_dropout/$step
 for i in *R1_001.fastq.gz
 do
     echo -e $(echo "$i" | cut -d "_" -f 1)","$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
-done | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/$step
+done | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/$step &
 cd ..
 
 # 06_merged
@@ -28,7 +28,7 @@ echo -e "$step" > ../10_read_dropout/$step
 for i in *.gz
 do
     echo -e $(echo "$i" | cut -d "_" -f 1)","$(echo $(gunzip -c "$i" | wc -l) / 4 | bc | cut -d "." -f 1)
-done | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/$step
+done | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/$step &
 cd ..
 
 # 07_split_amplicons
@@ -40,7 +40,7 @@ do
     for j in $(ls -1 *merged_"$i"*.gz | grep "$i")
     do
         echo -e $(echo "$j" | cut -d "_" -f 1)","$(echo $(gunzip -c "$j" | wc -l) / 4 | bc | cut -d "." -f 1)
-    done | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/"$step"_"$i"
+    done | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/"$step"_"$i" &
 done
 cd ..
 
@@ -53,7 +53,7 @@ do
     for j in $(ls -1 *merged_"$i"*_unique.fasta.gz | grep "$i")
     do
         echo -e $(echo "$j" | cut -d "_" -f 1)","$(echo $(gunzip -c "$j" | grep ">" | cut -d "_" -f 4 | awk '{s+=$1}END{print s}'))
-    done | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/"$step"_"$i"
+    done | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/"$step"_"$i" &
 done
 cd ..
 
@@ -65,6 +65,8 @@ do
     echo -e "$step" > ../10_read_dropout/"$step"_"$i"; awk -F "," 'NR==1 {for (i=1; i<=NF; i++) {headers[i]=$i}} NR>1 {for (i=1; i<=NF; i++) {sums[i]+=$i}} END {for (i=6; i<=NF; i++) {print headers[i]","sums[i]}}' "$i"_species_table.csv | sort -t "," -k1,1 -V | cut -d "," -f 2 >> ../10_read_dropout/"$step"_"$i"
 done
 cd ..
+
+wait
 
 # Put everything together
 step="10_read_dropout"
